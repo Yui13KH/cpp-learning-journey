a2743a1d54c2be83a80cc8b8b3f3d850a5b44985
Yui13KH <yuihirasawakh@hotmail.com>
2024-12-01 13:02:41 +0300

    Added enum conditionals example using switch case

--- Subjects/6-Conditional-Statements.cpp
+++ Subjects/6-Conditional-Statements.cpp
@@ -1,31 +1,34 @@
 #include <iostream>
 using namespace std;
 
+enum enWeekDays { MONDAY = 1, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY };
+
 int main() {
     int day;
     cout << "Enter a number between 1 and 7: ";
     cin >> day;
 
     if (day == 1) {
-        cout << "Monday";
+        cout << "Monday \n";
     } else if (day == 2) {
-        cout << "Tuesday";
+        cout << "Tuesday \n";
     } else if (day == 3) {
-        cout << "Wednesday";
+        cout << "Wednesday \n";
     } else if (day == 4) {
-        cout << "Thursday";
+        cout << "Thursday \n";
     } else if (day == 5) {
-        cout << "Friday";
+        cout << "Friday \n";
     } else if (day == 6) {
-        cout << "Saturday";
+        cout << "Saturday \n";
     } else if (day == 7) {
-        cout << "Sunday";
+        cout << "Sunday \n";
     } else {
-        cout << "Invalid day";
+        cout << "Invalid day \n";
     }
 
     // or you can use switch case too
 
+    /*
     switch (day) {
         case 1:
             cout << "Monday";
@@ -50,6 +53,42 @@ int main() {
             break;
         default:
             cout << "Invalid day";
+            break;
+    } */
+
+    // enums with switch case
+
+    enWeekDays dayOfWeek;
+    int enumValue;
+    cout << "Enter a number between 1 and 7, for enum:";
+    cin >> enumValue;
+    dayOfWeek = enWeekDays(enumValue);
+
+    // you can use else if's but switch case is more readable
+    switch (dayOfWeek) {
+        case MONDAY:
+            cout << "Monday \n";
+            break;
+        case TUESDAY:
+            cout << "Tuesday \n";
+            break;
+        case WEDNESDAY:
+            cout << "Wednesday \n";
+            break;
+        case THURSDAY:
+            cout << "Thursday \n";
+            break;
+        case FRIDAY:
+            cout << "Friday \n";
+            break;
+        case SATURDAY:
+            cout << "Saturday \n";
+            break;
+        case SUNDAY:
+            cout << "Sunday \n";
+            break;
+        default:
+            cout << "Invalid day \n";
             break;
     }
 
