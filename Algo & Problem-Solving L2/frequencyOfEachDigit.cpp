e20e16b8a97ed0f42ce12e34ed19d6f7437b1e5c
Yui13KH <yuihirasawakh@hotmail.com>
2024-12-18 01:35:14 +0300

    Improved solution to frequency of each digit problem

--- Algo & Problem-Solving L2/frequencyOfEachDigit.cpp
+++ Algo & Problem-Solving L2/frequencyOfEachDigit.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 // this is the way i 100% believe the instructor going to solve it
 
-// i was right instead tho the instructor didnt use an array , he ran the function immediatly in a the print function , checked if its over 0 
+// i was right instead tho the instructor didnt use an array , he ran the function immediatly in a
+// the print function , checked if its over 0
 
 int readNumber(string msg) {
     int number = 0;
@@ -15,36 +16,29 @@ int readNumber(string msg) {
     return number;
 }
 
-int countFrequency(int number, int frequencyNumber) {
-    int counter = 0;
+void countFrequency(int number, int numberFrequency[]) {
     int remainder = 0;
     while (number > 0) {
         remainder = number % 10;
         number /= 10;
-
-        if (remainder == frequencyNumber) counter++;
-    }
-    return counter;
-}
-
-void allFrequencies(int number, int numberValues[]) {
-    for (int i = 0; i < 10; i++) {
-        numberValues[i] = countFrequency(number, i);
+        numberFrequency[remainder] += 1;
     }
 }
 
-void printFrequencies(int numberValues[]) {
-    for (int i = 0; i < 10; i++) {
-        if (numberValues[i] > 0) {
-            cout << "The frequency of " << i << " is " << numberValues[i] << endl;
+void printAllFrequencies(int numberFrequency[]) {
+    for (int i = 0; i <= 9; i++) {
+        if (numberFrequency[i] <= 0) {
+            continue;
+        } else {
+            cout << "frequency of " << i << " is " << numberFrequency[i] << endl;
         }
     }
 }
 
 int main() {
-    int number = readNumber("Enter a positive number: ");
+    int number = readNumber("enter a number: ");
     int numberFrequencies[10] = {0};
 
-    allFrequencies(number, numberFrequencies);
-    printFrequencies(numberFrequencies);
+    countFrequency(number, numberFrequencies);
+    printAllFrequencies(numberFrequencies);
 }
