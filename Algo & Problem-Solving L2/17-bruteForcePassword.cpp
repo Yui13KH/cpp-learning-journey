f0a6b34f2bb0d67f4e491a53f9249ca959c14d1d
Yui13KH <yuihirasawakh@hotmail.com>
2024-12-20 16:37:24 +0300

    Added Soltuion to brute forcing

--- Algo & Problem-Solving L2/17-bruteForcePassword.cpp
+++ Algo & Problem-Solving L2/17-bruteForcePassword.cpp
@@ -11,8 +11,8 @@ bool isValidPassword(const string& str) {
         }
     }
     return true;
-} // simple check if its 3 letters long and in range
- 
+}  // simple check if its 3 letters long and in range
+
 string readInput(const string& msg) {
     string strNumber;
     bool validInput = false;
@@ -30,23 +30,25 @@ string readInput(const string& msg) {
     } while (!validInput);
 
     return strNumber;
-} 
+}
 
 void printAlphabetTriplet(string password) {
     string passwordCheck = "";
-    // most straight forward is 3 loops
+    int counter = 0;
+
+    // Nested loops to generate all triplets
     for (int i = 65; i <= 90; i++) {
         for (int j = 65; j <= 90; j++) {
             for (int k = 65; k <= 90; k++) {
+                counter++;
                 passwordCheck = string(1, char(i)) + char(j) + char(k);
+
                 if (passwordCheck == password) {
                     cout << "Password is " << passwordCheck << endl;
-                    cout << "Found after " << (i - 65) * 26 * 26 + (j - 65) * 26 + (k - 65) + 1
-                         << " Trials" << endl;
+                    cout << "Found after " << counter << " trials." << endl;
                     return;
                 } else {
-                    cout << "Trial[" << (i - 65) * 26 * 26 + (j - 65) * 26 + (k - 65) + 1
-                         << "]: " << passwordCheck << endl;
+                    cout << "Trial[" << counter << "]: " << passwordCheck << endl;
                 }
             }
         }
@@ -58,4 +60,5 @@ int main() {
     return 0;
 }
 
-// (i - 65) * 26 * 26 + (j - 65) * 26 + (k - 65) + 1 , doing this to calculate the entire number of trials 
+// (i - 65) * 26 * 26 + (j - 65) * 26 + (k - 65) + 1 , doing this to calculate the entire number of
+// trials
