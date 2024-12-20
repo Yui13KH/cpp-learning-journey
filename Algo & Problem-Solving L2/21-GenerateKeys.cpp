b33951d7de7d8c687e99a5f871765f523fea6a51
Yui13KH <yuihirasawakh@hotmail.com>
2024-12-20 23:22:54 +0300

    Improved generation of keys to be more random

--- Algo & Problem-Solving L2/21-GenerateKeys.cpp
+++ Algo & Problem-Solving L2/21-GenerateKeys.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <cstdlib>
 
-// ok after watching instructor's video i understand the 
+// ok after watching instructor's video he made use of enums to make the key more randomized
+// even though he didnt specify , he only said capital letters
+
+enum charType { CAPITAL_LETTER, SMALL_LETTER, DIGIT, SYMBOL };
 
 using namespace std;
 
@@ -16,15 +19,41 @@ int readInput(string msg) {
 
 int generateRandomNumber(int min, int max) { return rand() % (max - min + 1) + min; }
 
-char generateRandomChar() {
-    char c = char(generateRandomNumber(65, 90));
-    return c;
+char GenerateCapitalLetter() { return char(generateRandomNumber(65, 90)); }
+
+char GenerateSmallLetter() { return char(generateRandomNumber(97, 122)); }
+
+int GenerateDigit() {
+    int digit = generateRandomNumber(0, 9);
+    return digit;
+}
+
+char GenerateSymbol() {
+    int ranges[4][2] = {
+        {33, 47}, {58, 64}, {91, 96}, {123, 126}};  // ranges of ascii symbols check them out
+    int rangeIndex = generateRandomNumber(0, 3);            // gets one of the ranges randomly
+    return char(generateRandomNumber(ranges[rangeIndex][0], ranges[rangeIndex][1]));
+}
+
+char generateCharacter(charType type) {
+    switch (type) {
+        case CAPITAL_LETTER:
+            return GenerateCapitalLetter();
+        case SMALL_LETTER:
+            return GenerateSmallLetter();
+        case DIGIT:
+            return char(GenerateDigit() + '0');  // Convert digit to character
+        case SYMBOL:
+            return GenerateSymbol();
+        default:
+            throw std::invalid_argument("Invalid character type");
+    }
 }
 
 string generateWord(int length) {
     string word = "";
     for (int i = 0; i < length; i++) {
-        word += generateRandomChar();
+        word += generateCharacter(charType(generateRandomNumber(0, 3)));
     }
     return word;
 }
