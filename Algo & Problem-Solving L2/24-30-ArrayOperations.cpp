59f8be854902b0dbbfe2b345f678d4a4384846b4
Yui13KH <yuihirasawakh@hotmail.com>
2024-12-24 10:18:51 +0300

    Add Solution to Sum of Randomized Array

--- Algo & Problem-Solving L2/24-30-ArrayOperations.cpp
+++ Algo & Problem-Solving L2/24-30-ArrayOperations.cpp
@@ -44,6 +44,14 @@ int returnSmallestNumInArray(int randomizedArray[], int size) {
     return smallest;
 }
 
+int returnSumOfArray (int randomizedArray[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += randomizedArray[i];
+    }
+    return sum;
+}
+
 void printArrayElements(int randomizedArray[], int size) {
     cout << "Elements of array is: ";
     for (int i = 0; i < size; i++) {
