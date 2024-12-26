898a2404e671619c7cc26c4c958d72a8c39d0133
Yui13KH <yuihirasawakh@hotmail.com>
2024-12-26 12:46:23 +0300

    Solution to finding a value in a randomized array

--- Algo & Problem-Solving L2/34-FindValueInArray.cpp
+++ Algo & Problem-Solving L2/34-FindValueInArray.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <cstdlib>
 
-using namespace std;
+// using namespace std;  i am not using using namespace anymore cause it may cause confusion and its
+// not very good
 
-int readNumberInRange(string msg, int min, int max) {
+int readNumberInRange(std::string msg, int min, int max) {
     int number;
     do {
-        cout << msg;
-        cin >> number;
+        std::cout << msg;
+        std::cin >> number;
     } while (min > number || number > max);
     return number;
 }
@@ -20,12 +21,31 @@ void RandomizeArray(int array[], int arrayLength) {
     }
 }
 
-void printArrayKeys(int arrayOfKeys[], int arrayLenght) {
-    cout << "Elements of array are" << endl;
+void printArrayElements(int arrayOfKeys[], int arrayLenght) {
+    std::cout << "Elements of array are" << std::endl;
     for (int i = 0; i < arrayLenght; i++) {
-        cout << arrayOfKeys[i] << " ";
+        std::cout << arrayOfKeys[i] << " ";
     }
-    cout << endl;
+    std::cout << std::endl;
+}
+
+int searchInArray(const int array[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (array[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void printFoundValue(const int array[], int size, int target) {
+    int index = searchInArray(array, size, target);
+    if (index != -1) {
+        std::cout << "Value " << target << " found at index " << index << " (order: " << index + 1
+                  << ")." << std::endl;
+    } else {
+        std::cout << "Value " << target << " not found in the array." << std::endl;
+    }
 }
 
 int main() {
@@ -33,7 +53,9 @@ int main() {
     int array[100];
     int arrayLenght = readNumberInRange("Enter a number between 1-100: ", 1, 100);
     RandomizeArray(array, arrayLenght);
-    printArrayKeys(array, arrayLenght);
-
+    printArrayElements(array, arrayLenght);
+    int target = readNumberInRange("Enter Number To Search for between 1-100: ", 1, 100);
+    searchInArray(array, arrayLenght, target);
+    printFoundValue(array, arrayLenght, target);
     return 0;
 }
