7e2082ed22bd61a94f4aa052a8c562c090affaed
Yui13KH <yuihirasawakh@hotmail.com>
2024-12-31 15:42:40 +0300

    Problem 46: implement custom abs function

---
+++ Algo & Problem-Solving L2/46-customAbsoluteFunction.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int ReadValidNumber() {
+    int number = 0;
+    bool validInput;
+    do {
+        std::cout << "Enter a number: ";
+        std::cin >> number;
+
+        validInput = !std::cin.fail();
+        if (!validInput) {
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+            std::cout << "Invalid input. Please enter a valid integer.\n";
+        } else {
+            std::cin.ignore(1000, '\n');
+        }
+
+    } while (!validInput);
+    return number;
+}
+
+int absoluteValue(int number) {
+    if (number < 0) {
+        return -number;  // similar to number = number * -1;
+    } else {
+        return number;
+    }
+}
+
+int main() {
+    int number = ReadValidNumber();
+    std::cout << "custom absolute value of " << number << " is " << absoluteValue(number) << std::endl;
+    std::cout << "c++ absolute value of " << number << " is " << std::abs(number) << std::endl;
+    return 0;
+}
