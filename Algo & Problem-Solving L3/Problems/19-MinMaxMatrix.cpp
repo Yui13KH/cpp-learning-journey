75d9e16f47e059dfb10153c29cedd3f809964399
Yui13KH <yuihirasawakh@hotmail.com>
2025-01-18 16:11:42 +0300

    [Problem 19]: Find the Min and Max Numbers in a Matrix (Optimized with single pass)

---
+++ Algo & Problem-Solving L3/Problems/19-MinMaxMatrix.cpp
@@ -0,0 +1,46 @@
+#include "Utility.h"
+
+std::vector<int> flattenMatrix(Matrix& matrix) {
+    std::vector<int> flattenedMatrix;
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix[i].size(); j++) {
+            flattenedMatrix.push_back(matrix[i][j]);
+        }
+    }
+    return flattenedMatrix;
+}
+
+std::pair<int, int> findMinMax(std::vector<int>& flattenedMatrix) {
+    int min = flattenedMatrix[0];
+    int max = flattenedMatrix[0];
+
+    for (int i = 0; i < flattenedMatrix.size(); i++) {
+        if (flattenedMatrix[i] < min) {
+            min = flattenedMatrix[i];
+        }
+        if (flattenedMatrix[i] > max) {
+            max = flattenedMatrix[i];
+        }
+    }
+    return std::make_pair(min, max);
+}
+
+int main() {
+    srand(unsigned(time(NULL)));
+    int matrixRows = utility::getValidPositiveInt("Enter matrix rows: ");
+    int matrixColumns = utility::getValidPositiveInt("Enter matrix cols: ");
+
+    Matrix matrix = utility::generateRandomMatrix(matrixRows, matrixColumns, 1, 100);
+
+    utility::PrintMatrix(matrix, matrixRows, matrixColumns);
+
+    std::vector<int> flattenedMatrix = flattenMatrix(matrix);
+
+    std::pair<int, int> minMaxPair = findMinMax(flattenedMatrix);
+    std::cout << "Min: " << minMaxPair.first << std::endl;
+    std::cout << "Max: " << minMaxPair.second << std::endl;
+
+    return 0;
+}
+
+// command to compiler : g++ 19-MinMaxMatrix.cpp Utility.cpp -o name
