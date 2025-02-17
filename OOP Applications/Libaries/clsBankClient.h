e9ac837b601f60942b1f134f7adc4877c24d4ee5
Yui13KH <yuihirasawakh@hotmail.com>
2025-02-17 14:03:58 +0300

    Add Person Libary for inheritance

---
+++ OOP Applications/Libaries/clsBankClient.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include "../Libaries/clsString.h"
+#include "../Libaries/clsPerson.h"
+
+class clsBankClient : public clsPerson{
+private:
+  
+}
---
+++ OOP Applications/Libaries/clsPerson.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class clsPerson {
+   private:
+    std::string _FirstName;
+    std::string _LastName;
+    std::string _Email;
+    std::string _Phone;
+
+   public:
+    clsPerson(std::string FirstName, std::string LastName, std::string Email, std::string Phone) {
+        _FirstName = FirstName;
+        _LastName = LastName;
+        _Email = Email;
+        _Phone = Phone;
+    }
+
+    // getters
+    std::string FirstName() { return _FirstName; }
+    std::string LastName() { return _LastName; }
+    std::string Email() { return _Email; }
+    std::string Phone() { return _Phone; }
+
+    // setters
+    void FirstName(std::string FirstName) { _FirstName = FirstName; }
+    void LastName(std::string LastName) { _LastName = LastName; }
+    void Email(std::string Email) { _Email = Email; }
+    void Phone(std::string Phone) { _Phone = Phone; }
+};
