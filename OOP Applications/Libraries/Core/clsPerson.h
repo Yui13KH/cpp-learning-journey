58aaa183ddcb38b45cecb5f10e3f82dfe164b557
Yui13KH <yuihirasawakh@hotmail.com>
2025-03-23 00:46:21 +0300

    OOP BankSystem: Enforced structure using virtual methods and interfaces

--- OOP Applications/Libraries/Core/clsPerson.h
+++ OOP Applications/Libraries/Core/clsPerson.h
@@ -1,8 +1,9 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include "../Libraries/InterfaceCommunication.h"
 
-class clsPerson {
+class clsPerson : public InterfaceCommunication {
    private:
     std::string _FirstName;
     std::string _LastName;
@@ -29,4 +30,25 @@ class clsPerson {
     void SetLastName(const std::string& LastName) { _LastName = LastName; }
     void SetEmail(const std::string& Email) { _Email = Email; }
     void SetPhone(const std::string& Phone) { _Phone = Phone; }
+
+    // Using abstract classes and interfaces enforces a structured implementation, ensuring
+    // consistency and maintainability while allowing flexibility in how specific functionality is
+    // defined.
+
+    void SendEmail(std::string Title, std::string Body) override {
+        std::cout << "Sending Email: " << Title << " to " << _Email << std::endl;
+        std::cout << Body << std::endl;
+    }
+
+    void SendFax(std::string Title, std::string Body) override {
+        std::cout << "Sending Fax: " << Title << " to " << _Phone << std::endl;
+        std::cout << Body << std::endl;
+    }
+
+    void SendSMS(std::string Title, std::string Body) override {
+        std::cout << "Sending SMS: " << Title << " to " << _Phone << std::endl;
+        std::cout << Body << std::endl;
+    }
+
+    // just dummy functions to understand how it works
 };
