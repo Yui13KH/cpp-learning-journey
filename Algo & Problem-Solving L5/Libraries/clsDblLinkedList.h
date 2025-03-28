14534932594a2c573964739dacdf3d3fba0f12e4
Yui13KH <yuihirasawakh@hotmail.com>
2025-03-29 02:23:14 +0300

    Update: Added more operations to clsDblLinkedList

--- Algo & Problem-Solving L5/Libraries/clsDblLinkedList.h
+++ Algo & Problem-Solving L5/Libraries/clsDblLinkedList.h
@@ -3,28 +3,36 @@
 
 template <class T>
 class clsDblLinkedList {
-private:
+   private:
     int _listSize = 0;
 
-public:
+   public:
     class Node {
-    public:
+       public:
         T value;
         Node* next;
         Node* prev;
     };
 
     Node* head = nullptr;
+    Node* tail = nullptr;
+
+    int getListSize() const { return _listSize; }
+    void setListSize(int value) { _listSize = value; }
 
     void InsertAtBeginning(T value) {
-        Node* newNode = new Node{ value, head, nullptr };
-        if (head != nullptr)
+        Node* newNode = new Node{value, head, nullptr};
+        if (head != nullptr) {
             head->prev = newNode;
+        }
         head = newNode;
-       _listSize++;
+        if (tail == nullptr) {
+            tail = newNode;
+        }
+        _listSize++;
     }
 
-    void PrintList() {
+    void PrintList() const {
         Node* current = head;
         while (current != nullptr) {
             std::cout << current->value << " ";
@@ -33,11 +41,10 @@ public:
         std::cout << "\n";
     }
 
-    Node* Find(T value) {
+    Node* Find(T value) const {
         Node* current = head;
         while (current != nullptr) {
-            if (current->value == value)
-                return current;
+            if (current->value == value) return current;
             current = current->next;
         }
         return nullptr;
@@ -45,72 +52,79 @@ public:
 
     void InsertAfter(Node* current, T value) {
         if (!current) return;
-        Node* newNode = new Node{ value, current->next, current };
-        if (current->next != nullptr)
+        Node* newNode = new Node{value, current->next, current};
+        if (current->next != nullptr) {
             current->next->prev = newNode;
+        }
         current->next = newNode;
-       _listSize++;
+        if (newNode->next == nullptr) {
+            tail = newNode;
+        }
+        _listSize++;
+    }
+
+    bool InsertAfter(int index, T value) {
+        Node* current = getNodeByIndex(index);
+        if (current != nullptr) {
+            InsertAfter(current, value);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     void InsertAtEnd(T value) {
-        Node* newNode = new Node{ value, nullptr, nullptr };
+        Node* newNode = new Node{value, nullptr, tail};
+        if (tail != nullptr) {
+            tail->next = newNode;
+        }
+        tail = newNode;
         if (head == nullptr) {
             head = newNode;
         }
-        else {
-            Node* current = head;
-            while (current->next != nullptr) {
-                current = current->next;
-            }
-            current->next = newNode;
-            newNode->prev = current;
-        }
-       _listSize++;
+        _listSize++;
     }
 
     void DeleteNode(Node*& nodeToDelete) {
-        if (!head || !nodeToDelete) return;
-        if (head == nodeToDelete) head = nodeToDelete->next;
+        if (!nodeToDelete) return;
+        if (nodeToDelete == head) head = nodeToDelete->next;
+        if (nodeToDelete == tail) tail = nodeToDelete->prev;
+
         if (nodeToDelete->next) nodeToDelete->next->prev = nodeToDelete->prev;
         if (nodeToDelete->prev) nodeToDelete->prev->next = nodeToDelete->next;
+
         delete nodeToDelete;
-       _listSize--;
+        nodeToDelete = nullptr;
+        _listSize--;
     }
 
     void DeleteFirstNode() {
         if (!head) return;
         Node* temp = head;
         head = head->next;
-        if (head) head->prev = nullptr;
+        if (head)
+            head->prev = nullptr;
+        else
+            tail = nullptr;
         delete temp;
-       _listSize--;
+        _listSize--;
     }
 
     void DeleteLastNode() {
-        if (!head) return;
-        if (!head->next) {
-            delete head;
+        if (!tail) return;
+        Node* temp = tail;
+        tail = tail->prev;
+        if (tail)
+            tail->next = nullptr;
+        else
             head = nullptr;
-        }
-        else {
-            Node* current = head;
-            while (current->next->next != nullptr) {
-                current = current->next;
-            }
-            delete current->next;
-            current->next = nullptr;
-        }
-       _listSize--;
+        delete temp;
+        _listSize--;
     }
 
-    int Size() const {
-        return _listSize;
-    }
+    int Size() const { return _listSize; }
 
-    bool IsEmpty() {
-        // Returns the result of the condition itself, which evaluates to true or false
-        return head == nullptr;
-    }
+    bool IsEmpty() const { return head == nullptr; }
 
     void Clear() {
         Node* current = head;
@@ -119,9 +133,59 @@ public:
             current = current->next;
             delete temp;
         }
-        head = nullptr;
-        tail = nullptr;
+        head = tail = nullptr;
         _listSize = 0;
     }
 
+    void Reverse() {
+        Node* current = head;
+        Node* temp = nullptr;
+        while (current != nullptr) {
+            temp = current->prev;
+            current->prev = current->next;
+            current->next = temp;
+            current = current->prev;
+        }
+        if (temp != nullptr) {
+            head = temp->prev;
+        }
+    }
+
+    Node* getNodeByIndex(int index) {
+        if (index < 0 || index >= _listSize) return nullptr;
+
+        if (index < _listSize / 2) {
+            Node* current = head;
+            for (int i = 0; i < index; i++) {
+                current = current->next;
+            }
+            return current;
+        } else {
+            Node* current = tail;
+            for (int i = _listSize - 1; i > index; i--) {
+                current = current->prev;
+            }
+            return current;
+        }  // simple optimization , goes to index from closer end
+    }
+
+    T getValueByIndex(int index) {
+        Node* node = getNodeByIndex(index);
+
+        if (node != nullptr) {
+            return node->value;
+        } else {
+            return 0;
+        }
+    }
+
+    bool UpdateValueByIndex(int index, T value) {
+        Node* node = getNodeByIndex(index);
+        if (node != nullptr) {
+            node->value = value;
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
--- Algo & Problem-Solving L5/main.cpp
+++ Algo & Problem-Solving L5/main.cpp
@@ -1,32 +1,6 @@
 #include "Libraries/clsDblLinkedList.h"
 #include <iostream>
 int main() {
-    clsDblLinkedList<int> List;
 
-    if (List.IsEmpty()) {
-        std::cout << "\nlist is empty\n";
-    } else {
-        std::cout << "\nlist is not empty\n";
-    }
-
-    List.InsertAtBeginning(1);
-    List.InsertAtBeginning(2);
-    List.InsertAtBeginning(3);
-    List.InsertAtBeginning(4);
-    List.InsertAtBeginning(5);
-    List.DeleteFirstNode();
-
-    List.PrintList();
-    std::cout << "list size: " << List.Size();
-
-    if (List.IsEmpty()) {
-        std::cout << "\nlist is empty\n";
-    } else {
-        std::cout << "\nlist is not empty\n";
-    }
-
-    List.Clear();
-
-    std::cout << "list size: " << List.Size();
     return 0;
 }
