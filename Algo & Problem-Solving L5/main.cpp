c048e273cd32460ca7da0f17c1a47c351b3de265
Yui13KH <yuihirasawakh@hotmail.com>
2025-04-01 01:17:42 +0300

    Implemented Queue class using reusability

--- Algo & Problem-Solving L5/main.cpp
+++ Algo & Problem-Solving L5/main.cpp
@@ -1,6 +1,21 @@
 #include "Libraries/clsDblLinkedList.h"
+#include "Libraries/clsQueue.h"
 #include <iostream>
 int main() {
+    clsMyQueue<int> MyQueue;
+
+    MyQueue.push(1);
+    MyQueue.push(2);
+    MyQueue.push(3);
+    MyQueue.push(4);
+    MyQueue.push(5);
+    MyQueue.Print();
+
+    MyQueue.updateItem(2, 10);
+    MyQueue.Print();
+
+    MyQueue.clear();
+    MyQueue.Print();
 
     return 0;
 }
